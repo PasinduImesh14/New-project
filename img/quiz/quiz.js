2977ef753e3bde618049dedf129aa0e947c7bba7
Pasindu Imesh <pasinduimesh385@gmail.com>
2025-03-11 01:33:26 +0530

    new initial commit

---
+++ img/quiz/quiz.js
@@ -0,0 +1,52 @@
+const data = [
+    {
+        id: 1,
+        question: "What is your name?",
+        answers: [
+            {answer: "Amal", isCorrect: false},
+            {answer: "Saman", isCorrect: true},
+            {answer: "Nihal", isCorrect: false},
+            {answer: "Gune", isCorrect: false},
+        ],
+    },
+    {
+        id: 2,
+        question: "What is your age?",
+        answers: [
+            {answer: "13", isCorrect: false},
+            {answer: "21", isCorrect: false},
+            {answer: "18", isCorrect: true},
+            {answer: "75", isCorrect: false},
+        ],
+    },
+    {
+        id: 3,
+        question: "Where do you live?",
+        answers: [
+            {answer: "Colombo", isCorrect: false},
+            {answer: "Gampaha", isCorrect: false},
+            {answer: "Kurunegala", isCorrect: false},
+            {answer: "Pure", isCorrect: true},
+        ],
+    },
+    {
+        id: 4,
+        question: "What is your job?",
+        answers: [
+            {answer: "Teacher", isCorrect: false},
+            {answer: "Doctor", isCorrect: true},
+            {answer: "Engineer", isCorrect: false},
+            {answer: "Businessman", isCorrect: false},
+        ],
+    },
+    {
+        id: 5,
+        question: "What is your hobby?",
+        answers: [
+            {answer: "Playing", isCorrect: true},
+            {answer: "Reading", isCorrect: false},
+            {answer: "Sleeping", isCorrect: false},
+            {answer: "Riding", isCorrect: false},
+        ],
+    }
+];
