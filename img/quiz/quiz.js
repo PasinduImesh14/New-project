2d0be25188b171bde5a9e098a14cab7895e559c4
Pasindu Imesh <pasinduimesh385@gmail.com>
2025-03-12 02:03:57 +0530

    complete quiz

--- img/quiz/quiz.js
+++ img/quiz/quiz.js
@@ -20,7 +20,7 @@ const data = [
         ],
     },
     {
-        id: 3,
+        id: 1,
         question: "Where do you live?",
         answers: [
             {answer: "Colombo", isCorrect: false},
@@ -30,7 +30,7 @@ const data = [
         ],
     },
     {
-        id: 4,
+        id: 2,
         question: "What is your job?",
         answers: [
             {answer: "Teacher", isCorrect: false},
@@ -40,7 +40,7 @@ const data = [
         ],
     },
     {
-        id: 5,
+        id: 1,
         question: "What is your hobby?",
         answers: [
             {answer: "Playing", isCorrect: true},
@@ -48,5 +48,83 @@ const data = [
             {answer: "Sleeping", isCorrect: false},
             {answer: "Riding", isCorrect: false},
         ],
-    }
+    },
 ];
+  
+  const gameScreen = document.querySelector(".game");
+  const resultScreen = document.querySelector(".result");
+  const question = document.querySelector(".question");
+  const answersContainer = document.querySelector(".answers");
+  const submit = document.querySelector(".submit");
+  const play = document.querySelector(".play");
+  
+  let qIndex = 0;
+  let correctCount = 0;
+  let wrongCount = 0;
+  let total = 0;
+  let selectedAnswer;
+  
+  const playAgain = () => {
+    qIndex = 0;
+    correctCount = 0;
+    wrongCount = 0;
+    total = 0;
+    showQuestion(qIndex);
+  };
+  
+  play.addEventListener("click",()=>{
+    resultScreen.style.display = "none";
+    gameScreen.style.display = "block";
+    playAgain()
+  })
+  
+  const showResult = () => {
+    resultScreen.style.display = "block";
+    gameScreen.style.display = "none";
+  
+    resultScreen.querySelector(".correct").textContent = `Correct Answers: ${correctCount}`;
+  
+    resultScreen.querySelector(".wrong").textContent = `Wrong Answers: ${wrongCount}`;
+  
+    resultScreen.querySelector(".score").textContent = `Score: ${(correctCount - wrongCount) * 10}`;
+  };
+  
+  const showQuestion = (qNumber) => {
+    if (qIndex === data.length) return showResult();
+    selectedAnswer = null;
+    question.textContent = data[qNumber].question;
+    answersContainer.innerHTML = data[qNumber].answers
+      .map(
+        (item, index) =>
+          `
+    <div class="answer">
+        <input type="radio" id=${index} name="answer" value=${item.isCorrect} />
+        <label for="1">${item.answer}</label>
+    </div>
+    `
+      )
+      .join("");
+  
+    selectAnswer();
+  };
+  
+  const selectAnswer = () => {
+    answersContainer.querySelectorAll("input").forEach((el) => {
+      el.addEventListener("click", (e) => {
+        selectedAnswer = e.target.value;
+      });
+    });
+  };
+  
+  const submitAnswer = () => {
+    submit.addEventListener("click", () => {
+      if (selectedAnswer !== null) {
+        selectedAnswer === "true" ? correctCount++ : wrongCount++;
+        qIndex++;
+        showQuestion(qIndex);
+      } else alert("Select an answer!");
+    });
+  };
+  
+  showQuestion(qIndex);
+  submitAnswer();
